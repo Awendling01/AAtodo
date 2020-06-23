560ecdfb3bf05dff0302c2d83087d0e8a0d091de
Gohikeco1 <gohikeco1@gmail.com>
2020-06-23 16:49:29 -0500

    app reconstruction #2

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,52 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+const startingDate = new Date();
+type Todo = {
+  name: string;
+  completed: boolean;
+  dueDate: Date;
+};
+const todos: Todo[] = [
+  { name: "Clean Living Room", completed: false, dueDate: startingDate },
+  { name: "Dog Duty", completed: true, dueDate: startingDate },
+  { name: "Attend Meeting", completed: false, dueDate: startingDate },
+  { name: "DEF", completed: false, dueDate: startingDate },
+];
 
-function App() {
-  return <div className="App"></div>;
-}
+const todo = todos[0];
+const App = () => {
+  //body
+  return (
+    <div className="canvas">
+      <div className="todoList">
+        <div className="todo">
+          <div className="name">{todo.name}</div>
+          <div className="completed">
+            {todo.completed ? "Complete" : "Incomplete"}
+          </div>
+          <div className="dueDate"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
+// const App = () => {
+//   // function body
+//   const [theme, setTheme] = useState<"dark" | "light">("dark");
+//   const changeTheme = () => {
+//     theme === "dark" ? setTheme("light") : setTheme("dark");
+//   };
+//   return (
+//     <div
+//       className={`App fullscreen ${
+//         theme === "dark" ? "darkgray" : "lightgray"
+//       }`}
+//     >
+//       <h1 style={{ cursor: "pointer" }} onClick={changeTheme}>
+//         Hello Andrew!
+//       </h1>
+//     </div>
+//   );
+// };
 export default App;
