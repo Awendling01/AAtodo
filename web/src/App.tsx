c9fb2bee37e45d3d9d1d4abfb644300c121dc474
John <john@trabian.com>
2020-06-22 22:18:10 -0400

    Convert to useReducer/Context for state management

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,47 +1,125 @@
-import React from "react";
+import React, { useReducer, useContext } from "react";
 import "./App.css";
-import todos, { completedTodos } from "./utils/data";
+import todos from "./utils/data";
 import { TodoType } from "./utils/types";
+
+type AAState = {
+  todos: TodoType[];
+};
+
+const initialState: AAState = {
+  todos,
+};
+
+type ReducerAction = {
+  type: "markTodoComplete" | "markTodoIncomplete";
+  payload: TodoType;
+};
+
+const reducer = (state: AAState, action: ReducerAction) => {
+  const todo = action.payload;
+  switch (action.type) {
+    case "markTodoComplete":
+      return {
+        ...state,
+        todos: [
+          ...state.todos.filter((td) => td.id !== todo.id),
+          { ...todo, completed: true },
+        ],
+      };
+    case "markTodoIncomplete":
+      return {
+        ...state,
+        todos: [
+          ...state.todos.filter((td) => td.id !== todo.id),
+          { ...todo, completed: false },
+        ],
+      };
+    default:
+      throw new Error();
+  }
+};
 
 type TodoProps = {
   todo: TodoType;
 };
 
 const Todo: React.FC<TodoProps> = ({ todo }) => {
+  const { dispatch } = useContext(ReducerContext);
+  const markComplete = () => {
+    dispatch({ type: "markTodoComplete", payload: todo });
+  };
+  const markIncomplete = () => {
+    dispatch({ type: "markTodoIncomplete", payload: todo });
+  };
+  const toggleCompleted = () => {
+    todo.completed ? markIncomplete() : markComplete();
+  };
   return (
-    <div className="todo">
-      <div className="name todoText">{todo.name}</div>
-      <div className="dueDate todoText">{todo.dueDate.toDateString()}</div>
-      <div className="completed todoText">
-        {todo.completed ? "Complete" : "Incomplete"}
-      </div>
-    </div>
+    <ul className="todo">
+      <li className="todoColumn name">
+        <div className="todoText firstColumn">{todo.name}</div>
+      </li>
+      <li className="todoColumn dueDate">
+        <div className="todoText">{todo.dueDate.toDateString()}</div>
+      </li>
+      <li className="todoColumn completed">
+        <div
+          className="todoText lastColumn statusToggle link"
+          onClick={toggleCompleted}
+        >
+          {todo.completed ? "Complete" : "Incomplete"}
+        </div>
+      </li>
+    </ul>
   );
 };
 
+const ReducerContext = React.createContext<{
+  state: AAState;
+  dispatch: React.Dispatch<ReducerAction>;
+}>({ state: { todos: [] }, dispatch: () => {} });
+
 const App = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { todos } = state;
+  const incompleteTodos = todos.filter((todo) => !todo.completed);
+  const completedTodos = todos.filter((todo) => todo.completed);
+
   return (
-    <div className="App">
-      <div className="todoList">
-        <div className="todoHeader">
-          <div className="headerText">Todo</div>
-          <div className="headerText">Due Date</div>
-          <div className="headerText">Status</div>
+    <ReducerContext.Provider value={{ state, dispatch }}>
+      <div className="App">
+        <div className="todoList">
+          <div className="todoHeader">
+            <div className="tableTitle">Todos</div>
+            <div />
+            <div />
+          </div>
+          {incompleteTodos.length ? (
+            incompleteTodos.map((todo, todoIndex) => {
+              return <Todo key={`todo${todoIndex}`} todo={todo} />;
+            })
+          ) : (
+            <div className="todo">
+              <div>All Done!</div>
+            </div>
+          )}
+          {completedTodos.length ? (
+            <React.Fragment>
+              <div className="todoHeader">
+                <div className="tableTitle">Todones</div>
+                <div />
+                <div />
+              </div>
+              {completedTodos.map((todo, todoIndex) => (
+                <Todo key={`todone${todoIndex}`} todo={todo} />
+              ))}
+            </React.Fragment>
+          ) : null}
+          <button className="addButton">+</button>
         </div>
-        {todos.map((todo, todoIndex) => {
-          return <Todo key={`todo${todoIndex}`} todo={todo} />;
-        })}
-        <div className="todoHeader secondList">
-          <div className="headerText">Todo</div>
-          <div className="headerText">Due Date</div>
-          <div className="headerText">Status</div>
-        </div>
-        {completedTodos.map((todo, todoIndex) => {
-          return <Todo key={`todone${todoIndex}`} todo={todo} />;
-        })}
-        <button className="addButton">+</button>
       </div>
-    </div>
+    </ReducerContext.Provider>
   );
 };
 
