8d3a06bd3ce79126c4586783c2b2b4fd52dd1ad3
Gohikeco1 <gohikeco1@gmail.com>
2020-06-23 23:04:21 -0500

    convert to arrow function

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-function App() {
+const App = () => {
   return <div className="App"></div>;
-}
+};
 
 export default App;
