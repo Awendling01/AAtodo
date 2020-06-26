7429963bdf8a0865a4e1a04cc1745bb0eaafbcaf
Gohikeco1 <gohikeco1@gmail.com>
2020-06-25 22:41:24 -0500

    added app & todo component/styles.

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,55 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import todos, { completedTodos } from "./utils/data";
+import { TodoType } from "./utils/types";
+
+type TodoProps = {
+  todo: TodoType;
+  todoIndex: number;
+};
+
+const Todo: React.FC<TodoProps> = ({ todo, todoIndex }) => {
+  return (
+    <div className="todo">
+      <div className="name todoText">{todo.name}</div>
+      <div className="dueDate todoText">{todo.dueDate.toDateString()}</div>
+      <div className="completed todoText">
+        {todo.completed ? "Completed" : "Incomplete"}
+      </div>
+    </div>
+  );
+};
 
 const App = () => {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="todoList">
+        <div className="todoHeader">
+          <div className="headerText">ToDo</div>
+          <div className="headerText">DueDate</div>
+          <div className="headerText">Status</div>
+        </div>
+        {todos.map((todo, todoIndex) => {
+          return (
+            <Todo key={`todo${todoIndex}`} todo={todo} todoIndex={todoIndex} />
+          );
+        })}
+      </div>
+      <div className="todoList secondList">
+        <div className="todoHeader">
+          <div className="headerText">ToDo</div>
+          <div className="headerText">DueDate</div>
+          <div className="headerText">Status</div>
+        </div>
+        {completedTodos.map((todo, todoIndex) => {
+          return (
+            <Todo key={`todo${todoIndex}`} todo={todo} todoIndex={todoIndex} />
+          );
+        })}
+        <button className="addButton">+</button>
+      </div>
+    </div>
+  );
 };
 
 export default App;
