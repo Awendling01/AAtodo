044cf47740bdafb070b7d3bb423284986e0b8fcd
Gohikeco1 <gohikeco1@gmail.com>
2020-06-26 18:37:54 -0500

    updated colors/ styles

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,61 +1,6 @@
-// import React from "react";
-// import "./App.css";
-// import todos, { completedTodos } from "./utils/data";
-// import { TodoType } from "./utils/types";
-
-// type TodoProps = {
-//   todo: TodoType;
-//   todoIndex: number;
-// };
-
-// const Todo: React.FC<TodoProps> = ({ todo, todoIndex }) => {
-//   return (
-//     <div className="todo">
-//       <div className="name todoText">{todo.name}</div>
-//       <div className="dueDate todoText">{todo.dueDate.toDateString()}</div>
-//       <div className="completed todoText">
-//         {todo.completed ? "Completed" : "Incomplete"}
-//       </div>
-//     </div>
-//   );
-// };
-
-// const App = () => {
-//   return (
-//     <div className="App">
-//       <div className="todoList">
-//         <div className="todoHeader">
-//           <div className="headerText">ToDo</div>
-//           <div className="headerText">DueDate</div>
-//           <div className="headerText">Status</div>
-//         </div>
-//         {todos.map((todo, todoIndex) => {
-//           return (
-//             <Todo key={`todo${todoIndex}`} todo={todo} todoIndex={todoIndex} />
-//           );
-//         })}
-//       </div>
-//       <div className="todoList secondList">
-//         <div className="todoHeader">
-//           <div className="headerText">ToDo</div>
-//           <div className="headerText">DueDate</div>
-//           <div className="headerText">Status</div>
-//         </div>
-//         {completedTodos.map((todo, todoIndex) => {
-//           return (
-//             <Todo key={`todo${todoIndex}`} todo={todo} todoIndex={todoIndex} />
-//           );
-//         })}
-//         <button className="addButton">+</button>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default App;
 import React from "react";
 import "./App.css";
-import todos, { completedTodos } from "./utils/data";
+import todos from "./utils/data";
 import { TodoType } from "./utils/types";
 
 type TodoProps = {
@@ -76,6 +21,31 @@ const Todo: React.FC<TodoProps> = ({ todo, todoIndex }) => {
 };
 
 const App = () => {
+  // const upcomingTodos = todos.filter(
+  //   (todo) => Number(todo.dueDate) > Number(new Date())
+  // );
+
+  // const overdueTodos = todos.filter(
+  //   (todo) => Number(todo.dueDate) < Number(new Date())
+  // );
+
+  // const todaysTodos = todos.filter(
+  //   (todo) => Number(todo.dueDate) === Number(new Date())
+  // );
+
+  // const categoryNames = todos
+  //   .map((todo) => todo.category)
+  //   .filter((v, i, a) => a.indexOf(v) === i);
+
+  // const todosByCategory = categoryNames.map((categoryName) => ({
+  //   categoryName,
+  //   categoryTodos: todos.filter((todo) => todo.category === categoryName),
+  // }));
+
+  const incompleteTodos = todos.filter((todo) => !todo.completed);
+
+  const completedTodos = todos.filter((todo) => todo.completed);
+
   return (
     <div className="App">
       <div className="todoList">
@@ -84,7 +54,7 @@ const App = () => {
           <div className="headerText">DueDate</div>
           <div className="headerText">Status</div>
         </div>
-        {todos.map((todo, todoIndex) => {
+        {incompleteTodos.map((todo, todoIndex) => {
           return (
             <Todo key={`todo${todoIndex}`} todo={todo} todoIndex={todoIndex} />
           );
--- web/src/utils/data.ts
+++ web/src/utils/data.ts
@@ -5,9 +5,6 @@ const todos: TodoType[] = [
   { id: 1, name: "Wash dog", dueDate: new Date(), completed: false },
   { id: 2, name: "Clean house", dueDate: new Date(), completed: false },
   { id: 3, name: "Prospect gold", dueDate: new Date(), completed: true },
-];
-
-export const completedTodos: TodoType[] = [
   { id: 4, name: "Wash Car", dueDate: new Date(), completed: true },
   { id: 5, name: "Do Laundry", dueDate: new Date(), completed: true },
   { id: 6, name: "Clean Garage", dueDate: new Date(), completed: true },
